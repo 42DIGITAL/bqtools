3a8f243d7d3aed2f22e51cd3be501a94dee0b17f
Jonathan Rahn <jonathan.rahn@42digital.de>
2019-03-28 21:43:40 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+with open('requirements.txt') as f:
+    DEPENDENCIES = f.read().split('\n')
+
+setup(
+    name = 'bqorm',
+    version = VERSION,
+    description = 'Python Micro-ORM for BigQuery',
+    author = 'Jonathan Rahn',
+    author_email = 'jonathan.rahn@42digital.de',
+    url = 'https://github.com/42DIGITAL/bqorm',
+    packages = find_packages(exclude=['tests']),
+    install_requires=DEPENDENCIES,
+    extras_require={'test': ['pytest']},
+)
