e02b83186de822bc60df659a112cc413ac3824e9
Jonathan Rahn <jonathan.rahn@42digital.de>
2019-06-11 13:53:21 +0200

    always init data and schema as lists, #10 - and some minor improvements

--- tests/test_bqtools.py
+++ tests/test_bqtools.py
@@ -1,6 +1,6 @@
 from fourtytwo import bqtools
 
-def test_bqorm_construct_columns():
+def test_bqtools_construct_columns():
     schema = [
         {'name': 'number', 'field_type': 'INTEGER'},
         {'name': 'text', 'field_type': 'STRING'},
@@ -12,7 +12,7 @@ def test_bqorm_construct_columns():
     assert len(table.data) == 2
     assert len(table.rows()) == 4
 
-def test_bqorm_construct_rows_dicts():
+def test_bqtools_construct_rows_dicts():
     schema = [
         {'name': 'number', 'field_type': 'INTEGER'},
         {'name': 'text', 'field_type': 'STRING'},
@@ -28,3 +28,46 @@ def test_bqorm_construct_rows_dicts():
     assert len(table.schema) == 2
     assert len(table.data) == 2
     assert len(table.rows()) == 4
+
+def test_bqtools_append_list():
+    schema = [
+        {'name': 'number', 'field_type': 'INTEGER'},
+        {'name': 'text', 'field_type': 'STRING'},
+    ]
+    table = bqtools.BQTable(schema=schema)
+    table.append([[1, 'a'], [2, 'b']])
+    assert len(table.data) == 2
+    assert len(table.rows()) == 2
+
+def test_bqtools_append_dict():
+    schema = [
+        {'name': 'number', 'field_type': 'INTEGER'},
+        {'name': 'text', 'field_type': 'STRING'},
+    ]
+    table = bqtools.BQTable(schema=schema)
+    table.append([
+        {'number': 1, 'text': 'a'},
+        {'number': 2, 'text': 'b'}
+    ])
+    assert len(table.data) == 2
+    assert len(table.rows()) == 2
+
+def test_bqtools_append_schema():
+    schema = [
+        {'name': 'number', 'field_type': 'INTEGER'},
+        {'name': 'text', 'field_type': 'STRING'},
+    ]
+    rows = [
+        {'number': 1, 'text': 'a'},
+        {'number': 2, 'text': 'b'},
+        {'number': 3, 'text': 'c'},
+        {'number': 4, 'text': 'd'},
+    ]
+    table = bqtools.BQTable(schema=schema, data=rows)
+    
+    table.schema += [{'name': 'test', 'field_type': 'STRING'}]
+    table.schema = table.schema + [{'name': 'test', 'field_type': 'STRING'}]
+    assert len(table.schema) == 3
+    assert len(table.data) == 3
+    assert len(table.rows()) == 4
+    
