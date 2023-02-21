ff18307addf9d957cb0c90b0891120a085b8b56b
Shahzeb <shahzeb@42digital.de>
2023-02-21 16:22:10 +0100

    removed packages specific versions and field type in bq schema

--- fourtytwo/bqtools/__init__.py
+++ fourtytwo/bqtools/__init__.py
@@ -326,7 +326,9 @@ class BQTable(object):
         if isinstance(table_ref, str):
             table_ref = bigquery.TableReference.from_string(table_ref)
 
-        upload_source_format = 'json' if any([f._field_type in ['STRUCT', 'RECORD'] or f._mode=='REPEATED' for f in self.schema]) else 'csv'
+        # upload_source_format = 'json' if any([f._field_type in ['STRUCT', 'RECORD'] or f._mode=='REPEATED' for f in self.schema]) else 'csv'
+        # upload_source_format = 'json' if any([f._mode=='REPEATED' for f in self.schema]) else 'csv'
+        upload_source_format = 'csv'
         if upload_source_format == 'csv':
             tmpfile = 'tmpfile_{}.csv'.format(random.randint(1000,9999))
             self.to_csv(tmpfile, delimiter=',')
